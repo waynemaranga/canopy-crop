5e62462b094dcfc7aa293d16859b114a03ce62dc
WM <waynemaranga@gmail.com>
2025-02-08 00:47:34 +0300

    some-chores

--- src/tests/logs.py
+++ src/tests/logs.py
@@ -1,6 +1,10 @@
 import logging
 import datetime
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger: logging.Logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+    logger.debug(datetime.datetime.now().isoformat())
 
     print("\n🐬")
